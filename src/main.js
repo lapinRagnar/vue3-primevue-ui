6fc484a940d25426ea1048b752e3c730e56f6f95
lapinRagnar <lapinragnar@gmail.com>
2022-08-11 19:22:13 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+
+
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
+const app = createApp(App)
+
+app.use(store)
+app.use(router)
+app.use(PrimeVue)
+
+app.component('InputText', InputText)
+app.component('MonButton', Button)
+
+
+app.mount('#app')
